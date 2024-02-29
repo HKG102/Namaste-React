e75270dfb05c7d13101e3c9ad25325bca0c7008b
Harish Gupta <montygupta008@gmail.com>
2024-02-29 22:45:25 +0530

    Functional Component

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
-
-// React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-const heading = React.createElement("h1", {id: "heading"}, "Hi this is Heading.");
-console.log(heading);
-// JSX (transpiled before it reaches the JS Engine) - PARCEL transpiled it, using package Babel
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
 // JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
-const jsxHeading = <h1 id="heading">Namaste from JSX</h1>;
-console.log(jsxHeading);// both heading and jsxHeading are same.
+
+// React Element
+const jsxHeading = (
+<h1 className="head">Namaste from JSX</h1>
+);
+
+//React Component type:
+// 1. Class Based Component - OLD way
+// 2. Funtional component - NEW way
+
+
+// Functional Component (rturn a react eleement)
+
+const num = 6;
+
+const Title = () => (
+    <h1 className="head">Namaste from Title</h1>
+);
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1>Namaste {jsxHeading}  React from {num} Functional Component </h1>
+    </div>
+);
+
+// const HeadingComponent2 = () => (
+//     <h1 className="heading">Namaste React from Functional Component </h1>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// root.render(jsxHeading); //render react element 
+root.render(<HeadingComponent/>); // render functional component
