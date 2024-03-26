91325c18c0377a992b71c120e8e19fd52187ee47
Harish Gupta <montygupta008@gmail.com>
2024-03-26 16:44:42 +0530

    useEffect and useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,47 +1,66 @@
 import RestaurantCard from "./RestaurantCard";
-import resList  from "../utils/mockData";
-import {useState} from "react";
-
-
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  // Local state Variable - Super powerful variable
+  let [listOfREstaurants, setListOfRestaurant] = useState([]);
 
-    // Local state Variable - Super powerful variable
-    let arr = useState(resList);
-console.log(arr);
-    [listOfREstaurants, setListOfRestaurant] = arr;
+  // useEffect callback fucntion called after component is rendered
+  useEffect(() => {
+    fetchData();
+  }, []); // two argument -> callback function and dependency array
 
-
-    //Normal JS variable
-    //let listOfREstaurants = resList;
-    
-    return (
-      <div className="body">
-        <div className="filter">
-            <button className="filter-btn" 
-                    onClick={()=>{
-                        
-                        const filteredList = listOfREstaurants.filter((res) => res.data.avgRating > 4);
-                        console.log(filteredList);
-                        setListOfRestaurant(filteredList);
-                    }
-                        }>Top Rated REsstaurants
-            </button>
-            <button className="filter-btn" 
-                    onClick={()=>{                      
-                        setListOfRestaurant(resList);
-                    }
-                        }>Reset
-            </button>
-            </div>
-        <div className="res-container">
-          {listOfREstaurants.map((restaurant) => (
-            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-          ))}
-        </div>
-      </div>
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&page_type=DESKTOP_WEB_LISTING"
     );
+
+    const json = await data.json();
+
+    // optional chaining
+    const resListData =
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+
+    console.log(resListData);
+
+    setListOfRestaurant(resListData);
   };
 
-  export default Body;
-  
+  //Normal JS variable
+  //let listOfREstaurants = resList;
+
+  //Conditional Rendering
+  // if(listOfREstaurants.length === 0){
+  //     return <Shimmer/>;
+  // }
+
+  return listOfREstaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfREstaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            console.log(filteredList);
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated REsstaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfREstaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
